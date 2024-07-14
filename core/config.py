a5f8c54154d449a53d8b50ca7d13cf1baf17fd70
Tema <108786888+Bednyakov@users.noreply.github.com>
2024-07-14 15:54:59 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 class HamsterKombat:
     token: str = os.getenv('HAMSTER_TOKEN', 'default_token_if_not_set')
 
-    url: str = "https://api.hamsterkombat.io/clicker/sync"
+    url: str = "https://api.hamsterkombatgame.io/clicker/sync"
 
     headers: dict = {
         'User-Agent': 'Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.108 Mobile Safari/537.36',
